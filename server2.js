cf09402ea73fb875e144d395d4e89585ba71292f
curryj1 <curryj1@duq.edu>
2019-07-24 20:50:35 -0400

    fixing css again

--- server2.js
+++ server2.js
@@ -31,10 +31,10 @@ app.get('/libs/external_connect.js',(req,res)=>{
 	res.sendFile(path.resolve(__dirname +'/libs/external_connect.js'));
 });
 app.get('/css/all.css',(req,res)=>{
-	res.set('Content-Type', 'text/css')
+	res.set('Content-Type', 'text/css');
 	res.sendFile(path.resolve(__dirname+'/css/all.css'));
 });
 
 app.listen(port);
 
-//
+//You are connected to database "postgres" as user "postgres" via socket in "/var/run/postgresql" at port "5432".
