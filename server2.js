bb00b8e21b237a5b317b78c7b26c42a1cf9b3cfc
curryj1 <curryj1@duq.edu>
2019-08-06 14:36:21 -0400

    fixing to create all.css

--- server2.js
+++ server2.js
@@ -34,8 +34,8 @@ app.get('/external_api.js',(req,res)=>{
 app.get('/intern',(req,res)=>{
 	res.sendFile(path.resolve(__dirname+'/index.html'));
 });
-app.get('all.css',(req,res)=>{
-	res.sendFile(path.resolve(__dirname+'style.css'));
+app.get('/css/all.css',(req,res)=>{
+	res.sendFile(path.resolve(__dirname+'/css/all.css'));
 });
 
 //in conference.js there is a function called connect which needs roomname as an atribute.
